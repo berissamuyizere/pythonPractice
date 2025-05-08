671782d64f83efe43605f9fbd1c39af3b06b70bf
BerissaMuyizere <muyizereberissa@gmail.com>
2025-05-08 10:47:29 +0300

    balanced string

--- functions/test.py
+++ functions/test.py
@@ -4,7 +4,22 @@ def total_inventory(ArrProduct):
         productPrice=  product["price"]*product["quantity"]
         totalPrice+=productPrice
         
-    return totalPrice 
+    return totalPrice
 
-ArrProduct = [{"name":"rice", "price": 100, "quantity": 20},{"name":"cake", "price": 200, "quantity": 10},{"name":"maize", "price": 190, "quantity": 20}]
+ArrProduct = [{"name":"rice", "price": 600, "quantity": 20},{"name":"cake", "price": 800, "quantity": 10},{"name":"maize", "price": 190, "quantity": 20}]
 print(total_inventory(ArrProduct))
+
+
+def balanced_str(str):
+    vowels = ["a", "i","o","u", "e"]
+    vowelCout =0
+    consoCount =0
+    for char in str:
+        if char in vowels:
+            vowelCout+=1
+        else:
+            consoCount+=1   
+    return vowelCout == consoCount
+
+str = "munezer"
+print(balanced_str(str))
