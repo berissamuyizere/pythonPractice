f091393800851298a747196505b0052f592ec8e7
BerissaMuyizere <muyizereberissa@gmail.com>
2025-05-09 19:02:40 +0300

    count down

--- functions/conditions.py
+++ functions/conditions.py
@@ -4,7 +4,6 @@ def check_if_even(number):
 
 
 
-
 def check_even_or_odd(number):
     if number%2 == 0:
         print(f"{number} is even")
@@ -23,3 +22,27 @@ def check_divisibility(n):
         else:
             print(f"{x} is not divisible by 2,3,5")     
                
+def fizz_buz(number):
+    for i in range(1, number):
+        if i%3 ==0 and i%5 ==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("buzz")
+
+
+def count_down(start):
+    while start >=0:
+        print(f"count down at {start}")
+        start -=1
+
+
+def count_down_with_break(start, stop):
+    while start>=0:
+        print(f"count down counting at {start}")
+        if start == stop:
+            print(f"stopping at {stop}")
+            break
+        start -=1
+                 
