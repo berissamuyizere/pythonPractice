65526d05b10373a8b5997669ea7b6b330d5a652e
BerissaMuyizere <muyizereberissa@gmail.com>
2025-05-12 07:10:30 +0300

    lists

--- functions/test.py
+++ functions/test.py
@@ -23,3 +23,39 @@ def balanced_str(str):
 
 str = "munezer"
 print(balanced_str(str))
+
+def print_num(x):
+    for i in x:
+        # print(i)
+        print(i%7)
+
+
+x= [10,20,30,40]
+print_num(x)
+
+def list_traversal(y):
+    b = []
+    for num in y:
+        for i in num:
+            print(i)
+            b.append(i)
+    print(b)
+
+
+y = [[1,2,3],[4,5,6],[7,8,9,10]]
+list_traversal(y) 
+
+def list_complehension(x):
+    print([i**3 for i in x])
+    print([i%9 for i in x])
+
+x =[1,2,3,4,5]
+list_complehension(x)   
+
+def two_lists(z):
+    new= len(z)//2
+    print([i*3 for i in z[:new]])
+    print([i*3 for i in z[new:]])
+
+z = [0,1,2,3,4,5,6,7,8,9]
+two_lists(z)
